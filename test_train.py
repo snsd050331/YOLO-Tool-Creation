ee49b4fbb1c7e552581b98bd5db044fd46838821
chengjhong <snsd050331@gmail.com>
2025-08-29 09:51:39 +0800

    fix: Only save the programs I develop for yolov11

--- test_train.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11n.yaml")  # build a new model from YAML
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
-
-# Train the model
-results = model.train(data="data/cone.yaml", epochs=10, imgsz=640)
